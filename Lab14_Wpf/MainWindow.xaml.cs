ce17a215fd5fccde575805f6562cea118dec78a0
vermei2@outlook.com <vermei2@outlook.com>
2022-02-26 16:56:51 +0300

    Добавьте файлы проекта.

---
+++ Lab14_Wpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14_Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProductName = "Батон белый",
+                ProductImage = 1,
+                ProductPrice = 50,
+                ProductCategory = ProductCategorys.Food,
+            });
+
+            products.Add(new Product()
+            {
+                ProductName = "Будильник",
+                ProductImage = 2,
+                ProductPrice = 250,
+                ProductCategory = ProductCategorys.Appliances,
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Мармелад",
+                ProductImage = 3,
+                ProductPrice = 150,
+                ProductCategory = ProductCategorys.Food,
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
+
+
+
---
+++ Lab14_Wpf/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14_Wpf
+
+{
+    public enum ProductCategorys
+    {
+        Appliances,
+        Food
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductImage { get; set; }
+        public ProductCategorys ProductCategory { get; set; }
+
+        public override string ToString()
+        {
+            return ProductName;
+        }
+    }
+}
